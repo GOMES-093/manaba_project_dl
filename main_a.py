e82c8d0c069dccf4f72907efc7872edda0653763
nakagome <21020452+GOMES-093@users.noreply.github.com>
2020-10-26 17:43:19 +0900

    Add files via upload

--- main_a.py
+++ main_a.py
@@ -14,15 +14,14 @@ import glob
 # 201022-1033 : 講義名とプロジェクト名を抽出し、それをフォルダ名にするように。
 #        1049 : URLを実行時に入力出来るように。
 # 201026-1547 : 名前フォルダの前に連番をつけられるように。出席確認が捗る…？
+#        1710 : 学生ごとに提出物を入れるように。バージョンaにフォーク。
+#        1742 : 人数が減っても連番フォルダがおかしくならないように。glob最高。
 
 ### 設定項目 ###
 #
-# プロジェクト内の「提出物を確認」のURLをここに貼る
+# プロジェクト内の「チームスレッド」のURLをここに貼る(teamtopがurlにある)
 # 空にすると実行時に入力させる。ターミナルにコピペのが楽よね
 load_url     =  ""
-# 各個人の提出物をそれぞれのフォルダにまとめるか(True/False)
-# 一人当たり2つ以上のファイルがある場合はTrueのがいいです
-named_folder  = True
 # フォルダの先頭に連番(True/False)
 # 出席とかとるときは便利かも
 serial_folder = True
@@ -55,12 +54,10 @@ for i, x in enumerate(slist):
     #フォルダがなければ作る
     dir_path="./test/"+dir_name+"/"
     name=x
-    if serial_folder:
-        name = str(i)+"_"+x
-    name_path=dir_path+name+"/"
-    if not named_folder:
-        name_path = dir_path
-    if not os.path.exists(name_path):
+    if glob.glob(dir_path+"*"+name)==[]:
+        if serial_folder:
+            name = str(i)+"_"+x
+        name_path=dir_path+name+"/"
         os.makedirs(name_path)
 sub_link=soup.find('div',{'class':'contentbody-right'}).find('div',{'class':'rightmostbutton'}).select('a')[0]['href']
 print(sub_link)
@@ -84,8 +81,6 @@ for i,element in enumerate(chap.select("li"),start=1):    # その中のliタグ
         name_path=dir_path+name+"/"+pname+'/'
     else:
         name_path=name_path[0]+'/'+pname+'/'
-    if not named_folder:
-        name_path = dir_path
     if not os.path.exists(name_path):
         os.makedirs(name_path)
 
